09913bd74015094214c189ce451f338e683e76ed
Badhon7821 <bayozidbostamibadhon@gmail.com>
2022-12-27 03:23:41 -0800

    fetch recipe design complete

---
+++ src/components/RecipeItem.js
@@ -0,0 +1,13 @@
+import { useParams } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
+
+const RecipeItem = () => {
+  const { id } = useParams();
+  const { data: recipe, loading, error } = useFetch(id);
+
+  console.log(recipe);
+
+  return <div>RecipeItem</div>;
+};
+
+export default RecipeItem;
